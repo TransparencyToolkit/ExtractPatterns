06b105f3fd3e3ac2e787c4bd957ce4e358456d14
shidash <shidash@shidash.com>
2015-12-17 21:32:11 -0500

    First commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Extracts data matching these patterns-
+- Terms with over n characters in a row in ALLCAPS
+- Terms that match words on an extraction list
+- Comma separated lists with terms of 2 words or less
+- Merges in skills field (or other specified field)
---
+++ lib/extract_patterns.rb
@@ -0,0 +1,134 @@
+require 'json'
+require 'pry'
+require 'termextractor'
+
+class ExtractPatterns
+  def initialize(input, fields, match_name)
+    @input = JSON.parse(input)
+    @fields = fields
+    @match_name = match_name
+    @output = Array.new
+  end
+
+  # Split to find matches
+  def comma_list_matches(value)
+    if value
+      # Split on commas
+      list_items = value.split(",")
+
+      # Only get items under certain num of words
+      list_items.reject!{ |item| item.split(" ").length > 2 }
+
+      # Clean whitespace and ands
+      return list_items.map { |match| match.gsub(" and", "").gsub("and ", "").gsub(".", "").strip.lstrip }
+    end
+    return []
+  end
+
+  # Get words in ALLCAPS past certain length
+  def get_allcaps(value, length)
+    if length && value
+      # Get all matches
+      matches = value.scan(/\b(?:[A-Z]|\s){#{length},}\b/)
+      
+      # Remove matches that are too long
+      matches.reject!{|match| match.length > 100}
+      
+      # Remove whitespace
+      return matches.map{ |match| match.strip.lstrip }
+    end
+    return []
+  end
+
+  # Extract set terms
+  def find_known_terms(item, field, extract_list)
+    d = TermExtractor.new(JSON.pretty_generate([item]), [field], "extracted_codewords")
+    d.extractSetTerms(File.read(extract_list), ["codeword"], "case_sensitive")
+    return JSON.parse(d.getAllOutput).first["extracted_codewords"]
+  end
+
+  # Normalize and match synonyms and deduplicate
+  def normalize_results(extracted_raw, synonym_list)
+    synonyms = JSON.parse(File.read(synonym_list))
+    outarr = extracted_raw.dup
+
+    # Go through all extracted
+    extracted_raw.each do |extracted|
+      # Go through each item in synonym list
+      synonyms.each do |key, value|
+        value["codeword"].each do |word|
+          # Match found!
+          if word.downcase == extracted.downcase
+            outarr.delete(extracted)
+            outarr.push(key)
+          end
+        end
+      end
+    end
+
+    # Return deduplicated
+    return outarr.uniq
+  end
+
+  # Go through all items in JSON and fields to search
+  def search_fields(allcaps_length, extract_list, merge_field)
+    # Extract from each item
+    @input.each do |item|
+      item[@match_name] = Array.new
+      
+      @fields.each do |field|
+        # Extract list results, allcaps, and known codewords from each field
+        list_results = comma_list_matches(item[field])
+        allcaps_results = get_allcaps(item[field], allcaps_length)
+        merge_results = item[merge_field] ? item[merge_field] : []
+        known_terms_results = find_known_terms(item, field, extract_list)
+                             
+        # Merge results and post-process
+        item[@match_name] = item[@match_name] | normalize_results((allcaps_results | list_results | merge_results | known_terms_results),
+                                                                  extract_list)
+      end
+
+      # Push updated item out
+      @output.push(item)
+    end
+
+    return @output
+  end
+
+  # Return a ranked hash of the results
+  def ranked_hash_output(results)
+    # Make array of all results
+    allmatches = Array.new
+    results.each do |i|
+      i["tools_mentioned"].each do |match|
+        allmatches.push(match)
+      end
+    end
+
+    # Make ranked hash
+    rankedhash = Hash.new
+    allmatches.each do |match|
+      if rankedhash[match]
+        rankedhash[match] += 1
+      else
+        rankedhash[match] = 1
+      end
+    end
+    return rankedhash.sort_by{|k, v| v}
+  end
+end
+
+dir = "/home/shidash/Data/ICWATCH-Data/data/second_set"
+overalloutput = Array.new
+Dir.foreach(dir) do |file|
+  next if file == '.' or file == '..'
+  if !File.directory?(dir+"/"+file) && file.include?(".json") && !file.include?(".json.gpg")
+    e = ExtractPatterns.new(File.read(dir+"/"+file), ["description", "summary"], "tools_mentioned")
+    results = e.search_fields(6, "extract_list.json", "skills")
+    File.write(file.gsub(".json", "_extracted.json"), JSON.pretty_generate(results))
+#    overalloutput.concat(results)
+  end
+end
+
+#e = ExtractPatterns.new(File.read("MECWEDB.json"), ["description", "summary"], "tools_mentioned")
+#puts e.ranked_hash_output(overalloutput)
